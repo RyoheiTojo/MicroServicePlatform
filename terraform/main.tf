969a18ebfb3a4d86a2892435e7387825f5c0031e
Ryohei <ryohei.mmeve@gmail.com>
2019-12-01 20:32:17 +0900

    nomad

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,6 +57,16 @@ resource "aws_security_group_rule" "inbound_consul" {
     security_group_id = aws_security_group.score_hub_sg.id
 }
 
+resource "aws_security_group_rule" "inbound_nomad" {
+    type = "ingress"
+    from_port = 4646
+    to_port = 4646
+    protocol = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+
+    security_group_id = aws_security_group.score_hub_sg.id
+}
+
 resource "aws_security_group_rule" "inbound_ssh" {
     type = "ingress"
     from_port = 22
